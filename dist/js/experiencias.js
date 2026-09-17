e3242532f2ad51bd4201db150fba187f8cfeb4f2
renancodando <renancodando@gmail.com>
2026-09-17 12:08:42 -0300

    Adiciona carregador das novas experiências

---
+++ dist/js/experiencias.js
@@ -0,0 +1,6 @@
+const estilo=document.createElement('link');
+estilo.rel='stylesheet';
+estilo.href='/css/experiencias.css?v=20260917x1';
+document.head.append(estilo);
+import './constelacao.js';
+import './lingua-viva.js';
